a6a6c2d0bb1e807c5ed878d326f8389a1eeaa9bb
darkAngelCraft <alan_g@hotmail.it>
2017-03-05 15:05:05 +0100

    dos

--- main.py
+++ main.py
@@ -1,1 +1,52 @@
-//ciao
+import os
+import subprocess
+
+# e necessario far partire il programma dal terminale con i seguenti comandi:
+# sudo python main.py
+#cosi facendo si hanno i permessi di root
+
+print '\nselection:'
+int_option = None
+print '1) DOS Attack'
+print '2) mail (IMAP)'
+
+while int_option is None:
+
+    try:
+        option = raw_input()
+    except SyntaxError:
+        option = None
+
+    #dos attack
+    if option == '1':
+        int_option1 = None
+        print "1) ATTACKER"
+        print "2) IP-TABLES"
+
+        option1 = raw_input()
+        if int_option1 == '1':
+            print "insert ip target:"
+            ip = raw_input()
+            print os.system('sudo ping -f '+ip)
+        else:
+            os.system('sudo iptables -A INPUT -p tcp --dport 80 -m state --state NEW -m limit --limit 50/minute --limit-burst 200 -j ACCEPT')
+
+    elif option == '2':
+        int_option2 = None
+        print "1) send mail"
+        print "2) IP-TABLES"
+
+        option2 = raw_input()
+        if option2 == '1':
+            print "insert ip target:"
+            ip = raw_input()
+            print "mail sending..."
+
+
+
+
+
+
+
+
+
