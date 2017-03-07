e945790ef8c5611d2989a6864c9d9d214a6c74fa
darkAngelCraft <alan_g@hotmail.it>
2017-03-07 14:58:08 +0100

    modificata struttura

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import smtplib
 # sudo python main.py
 #cosi facendo si hanno i permessi di root
 
-print '\nselection:'
+print '\nwho are you?'
 int_option = None
-print '1) DOS Attack'
-print '2) mail (IMAP)'
+print '1) Attacker'
+print '2) Defender'
 
 while int_option is None:
 
@@ -17,31 +17,20 @@ while int_option is None:
     except SyntaxError:
         option = None
 
-    #dos attack
+    #attacker
     if option == '1':
         int_option1 = None
-        print "1) ATTACKER"
-        print "2) IP-TABLES"
+        print "1) Dos Attack"
+        print "2) Send Mail"
 
         option1 = raw_input()
         if int_option1 == '1':
             print "insert ip target:"
             ip = raw_input()
             print os.system('sudo ping -f '+ip)
-        else:
-            os.system('sudo iptables -A INPUT -p tcp --dport 80 -m state --state NEW -m limit --limit 50/minute --limit-burst 200 -j ACCEPT')
 
-    elif option == '2':
-        int_option2 = None
-        print "1) send mail"
-        print "2) IP-TABLES"
-
-        option2 = raw_input()
-        if option2 == '1':
-            print "insert ip target:"
-            ip = raw_input()
-            print "mail sending..."
-
+        elif int_option1 == '2':
+            print "sending mail.."
             server = smtplib.SMTP('mailfake.gmail.com', 587)
             server.starttls()
             server.login("mailfake@gmail.com", "mailfakepassword")
@@ -49,7 +38,19 @@ while int_option is None:
             msg = "YOUR MESSAGE!"
             server.sendmail("alan.guerzi@gmail.com", "alan.guerzi@gmail.com", msg)
             server.quit()
-        else:
+
+    #defender
+    elif option == '2':
+        int_option2 = None
+        print 'using iptables for:'
+        print "1) Dos Attack"
+        print "2) Send Mail"
+
+        option2 = raw_input()
+        if option2 == '1':
+            os.system('sudo iptables -A INPUT -p tcp --dport 80 -m state --state NEW -m limit --limit 50/minute --limit-burst 200 -j ACCEPT')
+
+        elif option2 == '2':
             os.system('sudo iptables -A INPUT -s 0.0.0.0 --dport 25 -j DROP')
 
 
